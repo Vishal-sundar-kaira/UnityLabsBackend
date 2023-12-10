0f56f3c57a107ea68316bbbc01f4b895d269ecdf
vishal kaira <kairavishal37@gmail.com>
2023-12-10 17:26:56 +0530

    Schema Created for all entities.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express=require('express')
-const mongoose=require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const cors =require("cors")
-const dotenv=require('dotenv')
-const app=express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
 dotenv.config();
 mongoose.set('strictQuery', true);
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 })
   .then(() => {
     console.log('Connected to the database');
@@ -16,16 +19,23 @@ mongoose.connect(process.env.MONGO, {
   .catch((error) => {
     console.error('Error connecting to the database:', error);
   });
-app.use(express.json())//first middleware allowing to send json from client side to server
-app.use(cookieParser())//second middleware for cookie for storing jwt token.
-app.use(cors({origin:"Enter your frontend origin",credentials:true}))
-app.use((err,req,res,next)=>{
-  const errorStatus=err.status||500
-  const errorMessage=err.message||"something went wrong"
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://your-frontend-origin", credentials: true }));
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
   return res.status(errorStatus).send(errorMessage);
-})
+});
 
 
-app.listen(5000,()=>{
-    console.log("backend server is running")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port ${PORT}`);
+});
